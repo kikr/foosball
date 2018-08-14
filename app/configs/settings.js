46c4cc4ae8ae2809fcb79be79483274c1139948e
kim <kimzero@hotmail.com>
2018-08-14 14:34:23 +0200

    Added some initial files and settings to work out the app structure like in https://medium.com/the-react-native-log/organizing-a-react-native-project-9514dfadaa0

---
+++ app/configs/settings.js
@@ -0,0 +1,16 @@
+// For development
+let FIREBASE_API_KEY = 'AIzaSyAGOUw0bgZgCOs-0IFGUf_h6sqF4xhxJbY';
+let FIREBASE_PROJECT_ID = 'reactive-learning';
+
+if (process.env.NODE_ENV === 'production') {
+  FIREBASE_API_KEY = '';
+  FIREBASE_PROJECT_ID = '';
+}
+
+export const settings = {
+  env: process.env.NODE_ENV,
+  FIREBASE_API_KEY,
+  FIREBASE_PROJECT_ID,
+};
+
+export default settings;
