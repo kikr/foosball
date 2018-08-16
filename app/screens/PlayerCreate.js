c2b62a9bd8fabca3fe59bde58e70ff8a1865d826
kim <kimzero@hotmail.com>
2018-08-16 10:25:52 +0200

    Fixed navigating automatically back after creating a player. Navigation is not passed to children by default: https://reactnavigation.org/docs/en/glossary-of-terms.html#screen-component

--- app/screens/PlayerCreate.js
+++ app/screens/PlayerCreate.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PlayerCreateForm from '../components/PlayerCreateForm';
 
-const PlayerCreateScreen = () => (
-  <PlayerCreateForm />
+// Navigation property is not automatically passed down to screens' children
+const PlayerCreateScreen = ({ navigation }) => (/* eslint-disable-line react/prop-types */
+  <PlayerCreateForm navigation={navigation} />
 );
 
 export default PlayerCreateScreen;
