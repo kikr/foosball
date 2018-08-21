ef208f74c0cb159e4574a6a061ae1688f04ce945
kim <kimzero@hotmail.com>
2018-08-21 10:11:43 +0200

    Lifted Match player picker state up to Form level. Now user can no longer select same player for both teams

--- app/components/MatchCreateForm/MatchCreateForm.js
+++ app/components/MatchCreateForm/MatchCreateForm.js
@@ -11,6 +11,7 @@ import MatchCollection from '../../api/MatchCollection';
 import MatchPlayerPicker from '../MatchPlayerPicker';
 import MatchScorePicker from '../MatchScorePicker';
 import { MatchBuilder } from '../../dto/Match';
+import PlayerCollection from '../../api/PlayerCollection';
 import styles from './styles';
 
 class MatchCreateForm extends React.Component {
@@ -18,19 +19,34 @@ class MatchCreateForm extends React.Component {
     super(props);
     const startDate = new Date();
     this.state = {
-      isCreating: false, startDate, endDate: new Date(), awayScore: 0, homeScore: 0,
+      isCreating: false,
+      startDate,
+      endDate: new Date(),
+      awayScore: 0,
+      homeScore: 0,
+      home: [],
+      away: [],
+      playerSelection: [],
     };
 
     this.matchBuilder = new MatchBuilder();
     this.matchBuilder.setStart(startDate); // Allows user to use default value of the date picker
 
     this.onCreateMatch = this.onCreateMatch.bind(this);
-    this.onChangeSelectedHomePlayers = this.onChangeSelectedHomePlayers.bind(this);
-    this.onChangeSelectedAwayPlayers = this.onChangeSelectedAwayPlayers.bind(this);
+    this.onDeselectAwayPlayer = this.onDeselectAwayPlayer.bind(this);
+    this.onDeselectHomePlayer = this.onDeselectHomePlayer.bind(this);
+    this.onSelectHomePlayer = this.onSelectHomePlayer.bind(this);
+    this.onSelectAwayPlayer = this.onSelectAwayPlayer.bind(this);
     this.onChangeMatchStartDate = this.onChangeMatchStartDate.bind(this);
     this.onChangeMatchEndDate = this.onChangeMatchEndDate.bind(this);
     this.onChangeAwayScore = this.onChangeAwayScore.bind(this);
     this.onChangeHomeScore = this.onChangeHomeScore.bind(this);
+  }
+
+  componentDidMount() {
+    new PlayerCollection().getPlayers().then((players) => {
+      this.setState({ playerSelection: players });
+    });
   }
 
   onCreateMatch() {
@@ -46,14 +62,6 @@ class MatchCreateForm extends React.Component {
 
         navigation.goBack();
       });
-  }
-
-  onChangeSelectedHomePlayers(selectedHomePlayers) {
-    this.matchBuilder.setHome(selectedHomePlayers);
-  }
-
-  onChangeSelectedAwayPlayers(selectedAwayPlayers) {
-    this.matchBuilder.setAway(selectedAwayPlayers);
   }
 
   onChangeMatchStartDate(startDate) {
@@ -79,13 +87,69 @@ class MatchCreateForm extends React.Component {
     this.matchBuilder.setHomeScore(homeScore);
   }
 
+  onDeselectAwayPlayer(playerId) {
+    let { away } = this.state;
+    const { playerSelection } = this.state;
+
+    away = away.filter((selectedPlayer) => {
+      if (selectedPlayer.getId() !== playerId) {
+        return true;
+      }
+      playerSelection.push(selectedPlayer);
+      return false;
+    });
+
+    this.matchBuilder.setAway(away);
+    this.setState({ away, playerSelection });
+  }
+
+  onDeselectHomePlayer(playerId) {
+    let { home } = this.state;
+    const { playerSelection } = this.state;
+
+    home = home.filter((selectedPlayer) => {
+      if (selectedPlayer.getId() !== playerId) {
+        return true;
+      }
+      playerSelection.push(selectedPlayer);
+      return false;
+    });
+
+    this.matchBuilder.setHome(home);
+    this.setState({ home, playerSelection });
+  }
+
+  onSelectHomePlayer(selectedPlayerId) {
+    let { home, playerSelection } = this.state;
+
+    home = home.concat(
+      playerSelection.filter(player => player.getId() === selectedPlayerId),
+    );
+    playerSelection = playerSelection.filter(player => player.getId() !== selectedPlayerId);
+
+    this.matchBuilder.setHome(home);
+    this.setState({ home, playerSelection });
+  }
+
+  onSelectAwayPlayer(selectedPlayerId) {
+    let { away, playerSelection } = this.state;
+
+    away = away.concat(
+      playerSelection.filter(player => player.getId() === selectedPlayerId),
+    );
+    playerSelection = playerSelection.filter(player => player.getId() !== selectedPlayerId);
+
+    this.matchBuilder.setAway(away);
+    this.setState({ away, playerSelection });
+  }
+
   enableLoading() {
     this.setState({ isCreating: true });
   }
 
   render() {
     const {
-      isCreating, startDate, endDate, awayScore, homeScore,
+      isCreating, startDate, endDate, awayScore, homeScore, home, away, playerSelection,
     } = this.state;
 
     if (isCreating) {
@@ -122,8 +186,8 @@ class MatchCreateForm extends React.Component {
           justifyContent: 'space-between',
         }}
         >
-          <MatchPlayerPicker placeHolder="Pick a player" onChangeSelectedPlayers={this.onChangeSelectedHomePlayers} />
-          <MatchPlayerPicker placeHolder="Pick a player" onChangeSelectedPlayers={this.onChangeSelectedAwayPlayers} />
+          <MatchPlayerPicker placeHolder="Pick a player" onDeselectPlayer={this.onDeselectHomePlayer} onSelectPlayer={this.onSelectHomePlayer} playerSelection={playerSelection} selectedPlayers={home} />
+          <MatchPlayerPicker placeHolder="Pick a player" onDeselectPlayer={this.onDeselectAwayPlayer} onSelectPlayer={this.onSelectAwayPlayer} playerSelection={playerSelection} selectedPlayers={away} />
         </View>
 
 
--- app/components/MatchPlayerPicker/MatchPlayerPicker.js
+++ app/components/MatchPlayerPicker/MatchPlayerPicker.js
@@ -1,98 +1,54 @@
 import React from 'react';
 import {
-  ActivityIndicator,
   Picker,
   Button,
   View,
   Text,
   FlatList,
 } from 'react-native';
-import PlayerCollection from '../../api/PlayerCollection';
 
-class MatchPlayerPicker extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { isLoadingPlayers: true, playerSelection: [], selectedPlayers: [] };
-  }
+const MatchPlayerPicker = (props) => {
+  const {
+    playerSelection, selectedPlayers, onDeselectPlayer, onSelectPlayer, placeHolder,
+  } = props;
 
-  componentDidMount() {
-    this.setState({ isLoadingPlayers: false });
+  return (
+    <View style={{ flex: 1 }}>
+      <Picker
+        style={{ margin: 5 }}
+        onValueChange={playerId => onSelectPlayer(playerId)}
+      >
+        <Picker.Item value="" label={placeHolder} />
+        {
+          playerSelection.map(player => (
+            <Picker.Item
+              key={player.getId()}
+              label={`${player.firstName} ${player.lastName}`}
+              value={player.getId()}
+            />
+          ))
+        }
+      </Picker>
 
-    new PlayerCollection().getPlayers().then((players) => {
-      this.setState({ playerSelection: players, isLoadingPlayers: false });
-    });
-  }
+      <FlatList
+        style={{ margin: 5 }}
+        keyExtractor={player => player.getId()}
+        data={selectedPlayers}
+        renderItem={({ item }) => (
+          <View>
+            <Text>
+              {`${item.firstName}`}
+            </Text>
+            <Button
+              title="Deselect"
+              onPress={onDeselectPlayer.bind(undefined, item.getId())}
+            />
+          </View>)}
+      />
+    </View>
 
-  onDeselectPlayer(playerId) {
-    let { selectedPlayers, playerSelection } = this.state;
-
-    playerSelection = playerSelection.concat(
-      selectedPlayers.filter(player => player.getId() === playerId),
-    );
-    selectedPlayers = selectedPlayers.filter(player => player.getId() !== playerId);
-
-    this.setState({ selectedPlayers, playerSelection });
-  }
-
-  onSelectPlayer(selectedPlayerId) {
-    let { selectedPlayers, playerSelection } = this.state;
-    const { onChangeSelectedPlayers } = this.props;
-
-    selectedPlayers = selectedPlayers.concat(
-      playerSelection.filter(player => player.getId() === selectedPlayerId),
-    );
-    playerSelection = playerSelection.filter(player => player.getId() !== selectedPlayerId);
-
-    onChangeSelectedPlayers(selectedPlayers);
-    this.setState({ selectedPlayers, playerSelection });
-  }
-
-  render() {
-    const { isLoadingPlayers, playerSelection, selectedPlayers } = this.state;
-    const { placeHolder } = this.props;
-
-    if (isLoadingPlayers) {
-      return <ActivityIndicator />;
-    }
-
-    return (
-      <View style={{ flex: 1 }}>
-        <Picker
-          style={{ margin: 5 }}
-          onValueChange={playerId => this.onSelectPlayer(playerId)}
-        >
-          <Picker.Item value="" label={placeHolder} />
-          {
-            playerSelection.map(player => (
-              <Picker.Item
-                key={player.getId()}
-                label={`${player.firstName} ${player.lastName}`}
-                value={player.getId()}
-              />
-            ))
-          }
-        </Picker>
-
-        <FlatList
-          style={{ margin: 5 }}
-          keyExtractor={player => player.getId()}
-          data={selectedPlayers}
-          renderItem={({ item }) => (
-            <View>
-              <Text>
-                {`${item.firstName}`}
-              </Text>
-              <Button
-                title="Deselect"
-                onPress={this.onDeselectPlayer.bind(this, item.getId())}
-              />
-            </View>)}
-        />
-      </View>
-
-    );
-  }
-}
+  );
+};
 
 export default MatchPlayerPicker;
