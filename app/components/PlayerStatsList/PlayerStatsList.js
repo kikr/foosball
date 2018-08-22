be4cd0eda86e01b94e4b6ce71cbbc3048ca19ce5
kim <kimzero@hotmail.com>
2018-08-22 14:56:23 +0200

    Reuse code

--- app/components/PlayerStatsList/PlayerStatsList.js
+++ app/components/PlayerStatsList/PlayerStatsList.js
@@ -26,71 +26,19 @@ class PlayerStatsList extends React.Component {
   }
 
   onSortByWins() {
-    const { players } = this.state;
-    const { ascend } = this.state;
-
-    if (ascend) {
-      players.sort(
-        (player1, player2) => player1.stats.wins - player2.stats.wins,
-      );
-    } else {
-      players.sort(
-        (player1, player2) => player2.stats.wins - player1.stats.wins,
-      );
-    }
-    // Notice toggling of the sort direction.
-    this.setState({ players, ascend: !ascend });
+    this.sortByNumericPlayerStat('wins');
   }
 
   onSortByLosses() {
-    const { players } = this.state;
-    const { ascend } = this.state;
-
-    if (ascend) {
-      players.sort(
-        (player1, player2) => player1.stats.losses - player2.stats.losses,
-      );
-    } else {
-      players.sort(
-        (player1, player2) => player2.stats.losses - player1.stats.losses,
-      );
-    }
-    // Notice toggling of the sort direction.
-    this.setState({ players, ascend: !ascend });
+    this.sortByNumericPlayerStat('losses');
   }
 
   onSortByGoalsScored() {
-    const { players } = this.state;
-    const { ascend } = this.state;
-
-    if (ascend) {
-      players.sort(
-        (player1, player2) => player1.stats.goalsScored - player2.stats.goalsScored,
-      );
-    } else {
-      players.sort(
-        (player1, player2) => player2.stats.goalsScored - player1.stats.goalsScored,
-      );
-    }
-    // Notice toggling of the sort direction.
-    this.setState({ players, ascend: !ascend });
+    this.sortByNumericPlayerStat('goalsScored');
   }
 
   onSortByGoalsConceded() {
-    const { players } = this.state;
-    const { ascend } = this.state;
-
-    if (ascend) {
-      players.sort(
-        (player1, player2) => player1.stats.goalsConceded - player2.stats.goalsConceded,
-      );
-    } else {
-      players.sort(
-        (player1, player2) => player2.stats.goalsConceded - player1.stats.goalsConceded,
-      );
-    }
-    // Notice toggling of the sort direction.
-    this.setState({ players, ascend: !ascend });
+    this.sortByNumericPlayerStat('goalsConceded');
   }
 
   onSortByPlayerName() {
@@ -119,6 +67,23 @@ class PlayerStatsList extends React.Component {
         this.getAllPlayers();
       },
     );
+  }
+
+  sortByNumericPlayerStat(statProperty) {
+    const { players } = this.state;
+    const { ascend } = this.state;
+
+    if (ascend) {
+      players.sort(
+        (player1, player2) => player1.stats[statProperty] - player2.stats[statProperty],
+      );
+    } else {
+      players.sort(
+        (player1, player2) => player2.stats[statProperty] - player1.stats[statProperty],
+      );
+    }
+    // Notice toggling of the sort direction.
+    this.setState({ players, ascend: !ascend });
   }
 
   render() {
